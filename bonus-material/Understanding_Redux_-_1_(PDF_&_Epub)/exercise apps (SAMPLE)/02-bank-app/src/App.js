7fe97375d7f63977672a219d92d9874017809960
ZackDinerstein <zackdhome@gmail.com>
2019-10-06 16:57:13 +0300

    Added deposit feature

--- bonus-material/Understanding_Redux_-_1_(PDF_&_Epub)/exercise apps (SAMPLE)/02-bank-app/src/App.js
+++ bonus-material/Understanding_Redux_-_1_(PDF_&_Epub)/exercise apps (SAMPLE)/02-bank-app/src/App.js
@@ -6,22 +6,41 @@ import { updateAmount } from './actions';
 import "./App.css";
 
 class App extends Component {
-  handleWithdrawal = (e) => {
-    const { withdrawamount, emptyaccount } = e.target.dataset,
-      { totalAmount } = store.getState();
-    let newAmount;
+  state = {
+    selectValue: 'withdraw'
+  }
+
+
+  handleUpdateAmount = (e) => {
+    let { howmuch, emptyaccount } = e.target.dataset,
+      { totalAmount } = store.getState(),
+      newAmount;
 
     if (emptyaccount) {
       store.dispatch(updateAmount(0));
       return
     }
 
-    newAmount = totalAmount - parseInt(withdrawamount, 10);
+    console.log(this.state.selectValue)
+
+    switch(this.state.selectValue){
+      case 'withdraw':
+        newAmount = totalAmount - parseInt(howmuch, 10);
+      case 'deposit':
+        newAmount = totalAmount + parseInt(howmuch, 10);
+    }
     store.dispatch(updateAmount(newAmount));
+  }
+
+  updateSelect = (e) => {
+    console.log(e.target.value);
+    this.setState({selectValue: e.target.value});
   }
 
   render() {
     const { totalAmount, username } = store.getState();
+    let interactionType = this.state.selectValue === 'withdraw' ? 'WITHDRAW' : "DEPOSIT";
+
     return (
       <div className="App">
         <img className="App__userpic" src={photographer} alt="user" />
@@ -31,17 +50,22 @@ class App extends Component {
           <p className="App__amount--info">Total Amount</p>
         </div>
 
+        <select value={this.state.selectValue} onChange={this.updateSelect}>
+          <option value="withdraw">Withdraw</option>
+          <option value="deposit">Deposit</option>
+        </select>
+
         <section className="App__buttons">
-          <button data-withdrawamount="10000" onClick={this.handleWithdrawal} >
-              WITHDRAW $10,000
+          <button data-howmuch="10000" onClick={this.handleUpdateAmount} >
+            {interactionType} $10,000
           </button>
-          <button data-withdrawamount="5000" onClick={this.handleWithdrawal} >
-              WITHDRAW $5,000
+          <button data-howmuch="5000" onClick={this.handleUpdateAmount} >
+            {interactionType} $5,000
           </button>
         </section>
 
-        <p className="App__giveaway" data-emptyaccount="true" onClick={this.handleWithdrawal}>
-            Give away all your cash to charity
+        <p className="App__giveaway" data-emptyaccount="true" onClick={this.handleUpdateAmount}>
+          Give away all your cash to charity
         </p>
       </div>
     );
